1df887e78b45e505dcc9e54f889da74418c3fdd9
Shivam <shekharshivam51@gnmail.com>
2025-01-07 11:43:38 +0530

    updated addulr and dbconfig

--- src/lib/db.config.ts
+++ src/lib/db.config.ts
@@ -1,9 +1,26 @@
+// import { PrismaClient } from "@prisma/client";
+
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+// export const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     log: ["query", "error"],
+//   });
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// export default prisma;
+
+
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;  // Make undefined possible
+};
 
-export const prisma =
-  globalForPrisma.prisma ||
+const prisma = 
+  globalForPrisma.prisma ?? 
   new PrismaClient({
     log: ["query", "error"],
   });
